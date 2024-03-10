662e088a0d70d56d54b6175ec91799b3158de520
wvssm <neoramiregi@gmail.com>
2024-03-11 02:53:08 +0900

    update: settings에 허용 호스트 추가

--- mapproject/settings.py
+++ mapproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.180.157.242',]
 
 
 # Application definition
